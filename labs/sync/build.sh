c83b3cecfed1c348b13120ba5c64d162b32cb24d
dup2X <dup2snow@gmail.com>
2016-05-31 18:48:59 +0800

    add sync demo

---
+++ labs/sync/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+go build -o labs-sync 
---
+++ labs/sync/idc_sync.go
@@ -0,0 +1,252 @@
+/*
+Copyright 2009-2016 Weibo, Inc.
+
+All files licensed under the Apache License, Version 2.0 (the "License");
+you may not use these files except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+// Queue Message
+type Message struct {
+	Key, Val string
+}
+
+// WQS Proxy
+type Proxy struct {
+	// should be replace
+	addr   string
+	others struct{}
+
+	// sync node
+	syncNode *Node
+}
+
+func NewProxy(readAddr string, peers []string) *Proxy {
+	node := &Node{
+		id:        readAddr,
+		in:        make(chan *Message, 1024),
+		out:       make(chan *Message, 1024),
+		reader:    NewHTTPReader(readAddr),
+		broadcast: newBroadcast(peers),
+		wg:        new(sync.WaitGroup),
+	}
+	return &Proxy{
+		addr:     readAddr,
+		syncNode: node,
+	}
+}
+
+func (p *Proxy) Start() {
+	// Service request, such as mc
+	http.Handle("/set", p)
+
+	// sync node workLoop
+	p.syncNode.Run()
+	select {}
+}
+
+func (p *Proxy) Push(k, v string) {
+	// handle client request
+	println(p.addr, "push", k, v)
+	msg := &Message{k, v}
+	// Write: local-storage
+	p.set(msg)
+
+	// Sync: broadcast msg to peers
+	p.syncNode.out <- msg
+}
+
+// store
+func (p *Proxy) set(msg *Message) {
+	println(p.addr, "set", msg.Key, msg.Val)
+}
+
+// proxy service handler
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	var msg = Message{}
+	err = json.Unmarshal(data, &msg)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	p.Push(msg.Key, msg.Val)
+}
+
+// sync node def
+type Node struct {
+	id string
+
+	// reader channel
+	in     chan *Message
+	reader Reader
+
+	// broadcast channel
+	out       chan *Message
+	broadcast *Broadcast
+
+	wg *sync.WaitGroup
+}
+
+type Reader interface {
+	Recv() <-chan *Message
+}
+
+type Broadcast struct {
+	peers map[uint32]*Peer
+
+	// send msg to peers
+	syncFunc func(*Peer, *Message)
+}
+
+type Peer struct {
+	ID   uint32
+	Addr string
+}
+
+func newBroadcast(addrs []string) *Broadcast {
+	broadcast := &Broadcast{
+		peers:    make(map[uint32]*Peer),
+		syncFunc: syncIDC,
+	}
+	for i := 0; i < len(addrs); i++ {
+		broadcast.peers[uint32(i)] = &Peer{
+			Addr: addrs[i],
+		}
+	}
+	return broadcast
+}
+
+type SyncWriter struct{}
+
+func syncIDC(p *Peer, msg *Message) {
+	c := http.Client{}
+	bf := &bytes.Buffer{}
+	json.NewEncoder(bf).Encode(msg)
+	req, _ := http.NewRequest("POST", p.Addr, bf)
+	c.Do(req)
+}
+
+func (n *Node) Run() {
+	go n.storeLoop()
+	go n.recvLoop()
+	go n.sendLoop()
+}
+
+func (n *Node) recvLoop() {
+	n.wg.Add(1)
+	defer n.wg.Done()
+
+	in := n.reader.Recv()
+	for {
+		select {
+		case msg := <-in:
+			println(n.id, "recv", msg.Key, msg.Val)
+			n.in <- msg
+		}
+	}
+}
+
+func (n *Node) storeLoop() {
+	n.wg.Add(1)
+	defer n.wg.Done()
+
+	for {
+		select {
+		case msg := <-n.in:
+			println(n.id, "set", msg.Key, msg.Val)
+		}
+	}
+}
+
+func (n *Node) sendLoop() {
+	n.wg.Add(1)
+	defer n.wg.Done()
+
+	for {
+		select {
+		case msg := <-n.out:
+			for id := range n.broadcast.peers {
+				println("sent to", n.broadcast.peers[id].Addr, msg.Key, msg.Val)
+				n.broadcast.syncFunc(n.broadcast.peers[id], msg)
+			}
+		}
+	}
+}
+
+func (n *Node) Close() {
+	n.wg.Wait()
+}
+
+type httpReader struct {
+	addr string
+	in   chan *Message
+}
+
+func NewHTTPReader(addr string) *httpReader {
+	reader := &httpReader{
+		addr: addr,
+		in:   make(chan *Message, 1024),
+	}
+	go reader.serve()
+	return reader
+}
+
+func (hr *httpReader) Recv() <-chan *Message {
+	return hr.in
+}
+
+func (hr *httpReader) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	var msg = Message{}
+	err = json.Unmarshal(data, &msg)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	hr.in <- &msg
+}
+
+func (hr *httpReader) serve() {
+	http.Handle("/sync", hr)
+	http.ListenAndServe(hr.addr, nil)
+}
+
+func main() {
+	var (
+		addr  string
+		hosts string
+	)
+	flag.StringVar(&addr, "a", ":10002", "")
+	flag.StringVar(&hosts, "h", "http://127.0.0.1:10003/sync;http://127.0.0.1:10004/sync", "")
+	flag.Parse()
+	proxy := NewProxy(addr, strings.Split(hosts, ";"))
+	proxy.Start()
+}
