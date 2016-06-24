d76f511d806e1e402905f981ad94fd3ac1e911d9
lrita <lrita@163.com>
2016-06-24 13:29:58 +0800

    modify stats command output

--- service/mc/command_stats.go
+++ service/mc/command_stats.go
@@ -49,30 +49,30 @@ func commandStats(q queue.Queue, tokens []string, r *bufio.Reader, w *bufio.Writ
 
 	if fields == 1 {
 		var rusage syscall.Rusage
+		syscall.Getrusage(0, &rusage)
 		getHits := metrics.GetCounter(metrics.CmdGet)
 		getMiss := metrics.GetCounter(metrics.CmdGetMiss)
 		setHits := metrics.GetCounter(metrics.CmdSet)
 		setMiss := metrics.GetCounter(metrics.CmdSetMiss)
+		conns := metrics.GetCounter(metrics.ReConn)
 
 		fmt.Fprintf(w, "STAT pid %d\r\n", os.Getpid())
 		fmt.Fprintf(w, "STAT uptime %d\r\n", q.GetUpTime())
 		fmt.Fprintf(w, "STAT time %d\r\n", time.Now().Unix())
-		fmt.Fprintf(w, "STAT version qservice %s\r\n", q.GetVersion())
+		fmt.Fprintf(w, "STAT version %s\r\n", q.GetVersion())
 		fmt.Fprintf(w, "STAT pointer_size %d\r\n", unsafe.Sizeof(r)*8)
-		fmt.Fprintf(w, "STAT curr_connections %d\r\n", metrics.GetCounter(metrics.ReConn))
+		fmt.Fprintf(w, "STAT rusage_user %d.%06d\r\n", rusage.Utime.Sec, rusage.Utime.Usec)
+		fmt.Fprintf(w, "STAT rusage_system %d.%06d\r\n", rusage.Stime.Sec, rusage.Stime.Usec)
+		fmt.Fprintf(w, "STAT curr_connections %d\r\n", conns)
 		fmt.Fprintf(w, "STAT total_connections %d\r\n", metrics.GetCounter(metrics.ToConn))
+		fmt.Fprintf(w, "STAT connection_structures %d\r\n", conns)
 		fmt.Fprintf(w, "STAT get_cmds %d\r\n", getHits+getMiss)
 		fmt.Fprintf(w, "STAT get_hits %d\r\n", getHits)
 		fmt.Fprintf(w, "STAT set_cmds %d\r\n", setHits+setMiss)
 		fmt.Fprintf(w, "STAT set_hits %d\r\n", setHits)
 		fmt.Fprintf(w, "STAT bytes_read %d\r\n", metrics.GetCounter(metrics.BytesRead))
 		fmt.Fprintf(w, "STAT bytes_written %d\r\n", metrics.GetCounter(metrics.BytesWriten))
-		if err := syscall.Getrusage(0, &rusage); err == nil {
-			fmt.Fprintf(w, "STAT rusage_user %d.%06d\r\n", rusage.Utime.Sec, rusage.Utime.Usec)
-			fmt.Fprintf(w, "STAT rusage_system %d.%06d\r\n", rusage.Stime.Sec, rusage.Stime.Usec)
-		}
 		fmt.Fprintf(w, "STAT threads %d\r\n", runtime.NumGoroutine())
-		fmt.Fprintf(w, "STAT enable_set_cmd 1\r\n")
 		fmt.Fprint(w, respEnd)
 	} else if fields == 2 && tokens[1] == "queue" {
 		accumulationInfos, err := q.AccumulationStatus()
